2697496ae38ab73262282b6999e58bdf24c5d10c
Aiken Ong <aiken.ong@u.nus.edu>
2025-01-17 09:26:44 +0800

    first commit

---
+++ src/test/java/vttp/batchb/paf/day25/mini/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batchb.paf.day25.mini.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
